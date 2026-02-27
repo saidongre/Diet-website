d0aa09dea75db6ec0dcf59acfc62af0e772cdbe7
saidongre <sairamdongre2003@gmail.com>
2026-02-27 16:49:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+    $('.buttons').click(function(){
+        
+        $(this).addClass('active').siblings().removeClass('active');
+        
+        var filter = $(this).attr('data-filter')
+        
+        if(filter== 'all'){
+            
+            $('.diet .box').show(400);
+        }
+        else{
+            $('.diet .box').not('.'+ filter).hide(200);
+            $('.diet .box').filter('.'+ filter).show(400);
+        }
+    });
+});
+
+var swiper = new Swiper('.review-slider', {
+    loop: true,
+    grabCursor:true,
+    spaceBetween:20,
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        640:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:3,
+        },
+    },
+});
